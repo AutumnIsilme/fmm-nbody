ac3757126c2f891fe8a114693a5b86746b2614fb
Elijah Cann <Elijah@mbp-1214.wlan.dur.ac.uk>
2026-08-11 14:48:28 +0100

    Add cloud to a namespace, comment out dead code

--- Cpp-Translation/include/cloud/Cloud.h
+++ Cpp-Translation/include/cloud/Cloud.h
@@ -3,6 +3,8 @@
 #include <string>
 #include <vector>
 
+namespace fmm {
+
 std::vector<std::string> lines(const std::vector<std::vector<double>> &bodies);
 
 void write_points_and_masses(const std::vector<std::vector<double>> &bodies,
@@ -14,3 +16,5 @@ generate_points_and_masses_uniform_random_distribution(int n_points,
 
 std::vector<std::vector<double>>
 generate_2d_bodies_uniform_random(int n_points, double max_mass);
+
+} // namespace fmm
